a02b87f4502fcfc9b672befcd530e0b6a65f5901
Charlie Penner <cpenner461@karrotstick.net>
2013-10-19 16:38:31 -0500

    make sha1 the default password

--- review_app/review_app/settings/base.py
+++ review_app/review_app/settings/base.py
@@ -272,3 +272,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'PAGINATE_BY': 10
 }
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
