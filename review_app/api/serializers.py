8784265b45a628cd9d183361a6a6255b3861e556
Jared Bunting <jared.bunting@peachjean.com>
2013-10-19 20:31:48 -0500

    Adding demo structure.

--- review_app/api/serializers.py
+++ review_app/api/serializers.py
@@ -1,10 +1,15 @@
-from review_app.models import ReviewSession, FeedbackItem, ReviewUser
+from review_app.models import ReviewSession, FeedbackItem, ReviewUser, Demo
 from rest_framework import serializers
 
 
 class ReviewSessionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ReviewSession
+
+
+class DemoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Demo
 
 
 class FeedbackItemSerializer(serializers.HyperlinkedModelSerializer):
